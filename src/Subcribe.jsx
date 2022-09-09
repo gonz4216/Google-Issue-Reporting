7a2459ab9db2f35bd19ad87aba1b24ffe691fd78
gonz4216 <ethgon19@gmail.com>
2022-09-09 00:59:19 -0400

    main

--- src/Subcribe.jsx
+++ src/Subcribe.jsx
@@ -1,8 +1,26 @@
+import { Button } from '@nextui-org/react'
 import React from 'react'
-
+import { useState } from 'react'
+import Mail from './img/env.svg'
 const Subcribe = () => {
+ 
   return (
-    <div className='sub'>Subcribe</div>
+    <div className='sub'>
+      <div className="row">
+        <div className="container-mail">
+          
+          <img src={Mail} alt="" className="mail-logo"/>
+          <form action="">
+     
+            <input type="text" name='mail' placeholder='Email Address'/>
+            <Button className='btn-mail'>Submit</Button>
+            
+          </form>
+          
+        </div>
+      </div>
+
+    </div>
   )
 }
 
